6faf886764c0a48b20700521e723de7b48d723df
NellyGilA <86023328+NellyGilA@users.noreply.github.com>
2023-11-29 16:40:12 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+#Importar el módulo Flask en el proyecto.
+from flask import Flask
+
+#Crear un objeto de la clase Flask.
+app = Flask(__name__)
+
+#La función route() de la clase Flask.
+@app.route("/")
+
+#‘/’ URL está vinculado con la función first_flask.
+def first_flask():
+
+    return "Este es mi primer programa en Flask"
+
+#Definir una función con diferente punto final.
+
+
+#Ejecutamos la app en el servidor local.
+app.run()
